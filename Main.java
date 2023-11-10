2a1f82d4d38e6ab62dbead003c0f7faaee17cb91
user.email <developerjenzii@gmail.com>
2023-11-10 18:22:34 +0100

    touch some multidimensional array

--- Main.java
+++ Main.java
@@ -3,6 +3,16 @@ public class Main {
   public static void main(String args[]) {
     Calc calculator = new Calc();
     int res = calculator.add(3, 4);
-    System.out.println(res);
+    // System.out.println(res);
+
+    int[][] multiDimensionalArr = new int[3][4];
+
+    for (int i = 0; i < multiDimensionalArr.length; i++) {
+      for (int j = 0; j < multiDimensionalArr[0].length; j++) {
+        multiDimensionalArr[i][j] = (int) (Math.random() * 10);
+        System.out.print(multiDimensionalArr[i][j] + " ");
+      }
+      System.out.println();
+    }
   };
 };
